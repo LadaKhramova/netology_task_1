89676e520482ac6dc039d1c2fe05a3038a4976ed
Lada Khramova <khramovalada@gmail.com>
2022-11-29 13:33:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 5_000;
+        int mile = 20;
+
+        int result = amount / mile;
+
+        System.out.println("Количество начисленных миль: " + result);
+    }
+}
